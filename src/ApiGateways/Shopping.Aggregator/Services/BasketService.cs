44200382c501f611df4e9cb953da61f803a4e896
Paweł Salicki <pawel.salicki@gmail.com>
2022-12-16 10:38:23 +0100

    aggregator services and shopping controller

--- src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -1,6 +1,21 @@
-﻿namespace Shopping.Aggregator.Services
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
     }
 }
